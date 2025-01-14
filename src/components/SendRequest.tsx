5f112d574b71ff2ca65fc8fe9b4bcf189e8247c9
kareemrogers-coder <kareemrogers27@gmail.com>
2025-01-13 22:01:12 -0500

    commit 2

--- src/components/SendRequest.tsx
+++ src/components/SendRequest.tsx
@@ -1,3 +1,27 @@
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// // component to send JWT to backend and validate user.
+
+// const SendRequest = () => {
+//   const { getAccessTokenSilently } = useAuth0();
+
+//   const fetchProtectedData = async () => {
+//     const token = await getAccessTokenSilently();
+//     console.log(token);
+//     const response = await fetch("https://aura-library.onrender.com/users/login", {
+//       //this endpoint will be different, im just using the localhost as a placeholder
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//   };
+//   return <button onClick={() => fetchProtectedData()}>SendRequest</button>;
+// };
+
+// export default SendRequest;
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 // component to send JWT to backend and validate user.
@@ -7,12 +31,19 @@ const SendRequest = () => {
 
   const fetchProtectedData = async () => {
     const token = await getAccessTokenSilently();
-    console.log(token);
+    console.log("JWT from Auth0:", token);
     const response = await fetch("https://aura-library.onrender.com/users/login", {
       //this endpoint will be different, im just using the localhost as a placeholder
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json', 
+        //new
       },
+      body: JSON.stringify({ //new
+        auth0_token: token
+        //new
+      }) //new
     });
     const data = await response.json();
     console.log(data);
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,21 @@
 
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+// import { Auth0Provider } from '@auth0/auth0-react'
+
+// createRoot(document.getElementById('root')!).render(
+//   <Auth0Provider
+//     domain="dev-3niskqr7oyd1o1x3.us.auth0.com"
+//     clientId={"70cbN0Hq4XwBTiV5cscF5euIxmsJrCEl"}
+//     authorizationParams={{
+//       redirect_uri: window.location.origin
+//     }}
+//   >
+//     <App />
+//   </Auth0Provider>,
+// )
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,10 +23,12 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain="dev-3niskqr7oyd1o1x3.us.auth0.com"
-    clientId={"70cbN0Hq4XwBTiV5cscF5euIxmsJrCEl"}
+    domain="dev-y870izsbr3wx4epg.us.auth0.com"
+    clientId={"Zpc4qwTgZShW82WUmnAZtwQpHS9OjXck"}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: "this-is-our-logo-generator-app-project.",
+      // scope: "read:user"
     }}
   >
     <App />
